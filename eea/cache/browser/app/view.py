60faea27611d9c382bf45bbcca9382eb4c6042a6
Alexandru Ghica <alecghica@gmail.com>
2018-05-15 14:55:14 +0000

    replaced invalidate_cache method with a new method, called cache.invalidate.footer, that use eea.cache. refs #95020

--- eea/cache/browser/app/view.py
+++ eea/cache/browser/app/view.py
@@ -125,6 +125,55 @@ class InvalidateCache(BrowserView):
         return _(u"Varnish and Memcache invalidated.")
 
 
-class InvalidateCachePublic(BrowserView):
-    """ View to invalidate Varnish and Memcache
+class InvalidateCacheFooter(InvalidateCache):
+    """ Public view to invalidate Varnish and Memcache
     """
+
+    def __call__(self, **kwargs):
+        from Products.statusmessages.interfaces import IStatusMessage
+        from zope.component import getMultiAdapter
+        from Acquisition import aq_parent
+        from Products.CMFCore.utils import getToolByName
+
+        msg_invalidated = _(u"Cache invalidated.")
+        msg_not_invalidated = _(u"Cache could not be invalidated.")
+
+        # Get propper context for a default view
+        self.parent = self.context
+        state = getMultiAdapter((self.context, self.request), name='plone_context_state')
+        if state.is_default_page():
+            self.parent = aq_parent(self.context)
+
+        # Don't allow invalidation with direct link
+        referer = self.request.get('HTTP_REFERER', '')
+        if referer.endswith('/'):
+            referer = referer[:-1]
+        if (self.context.absolute_url() != referer and
+                    self.parent.absolute_url() != referer):
+            return msg_not_invalidated
+
+#        # Authenticated editors can invalidate cache from everywhere
+#        mtool = getToolByName(self.context, 'portal_membership')
+#        if mtool.checkPermission('Modify portal content', self.parent):
+#            return True
+
+#        # Check eea internal ips
+#        addr_list = set(['127.0.0.1'])
+#        ptool = getToolByName(self.context, 'portal_properties')
+#        ips_list = getattr(ptool, 'eea_internal_ips', None)
+#        if ips_list:
+#            addr_list.update(ips_list.getProperty('allowed_ips', []))
+
+#        addr = self.request.get('HTTP_X_FORWARDED_FOR', '')
+#        addr = addr or self.request.get('REMOTE_ADDR', '')
+
+#        for ip in addr_list:
+#            if addr.startswith(ip):
+#                return True
+#        return False
+
+        super(InvalidateCacheFooter, self).__call__(**kwargs)
+
+        IStatusMessage(self.request).addStatusMessage(msg_invalidated, type='info')
+        self.request.response.redirect(self.parent.absolute_url())
+        return msg_invalidated
