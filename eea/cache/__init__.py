35e3c7dd24ce172430bdc0a89c0bab79b7191123
Alin Voinea <contact@avoinea.com>
2023-03-17 22:18:08 +0200

    Revert "Fallback to plone.memoize choose_cache if MEMCACHE_SERVER env not provided"
    This reverts commit 4c376c60d4c23f7847c58fb3baf7424e380c225c.

--- eea/cache/__init__.py
+++ eea/cache/__init__.py
@@ -7,7 +7,6 @@ from plone.memoize import volatile
 from plone.memoize.interfaces import ICacheChooser
 from plone.memoize.ram import AbstractDict
 from plone.memoize.ram import store_in_cache
-from plone.memoize.ram import choose_cache as memoize_choose_cache
 from plone.uuid.interfaces import IUUID
 from eea.cache.utility import MemcachedClient
 from eea.cache.interfaces import IMemcachedClient
@@ -96,11 +95,8 @@ def frontpageMemcached():
 def choose_cache(fun_name):
     """ Choose cache
     """
-    if os.environ.get("MEMCACHE_SERVER"):
-        client = queryUtility(IMemcachedClient)
-        return MemcacheAdapter(client, globalkey=fun_name)
-    else:
-        return memoize_choose_cache(fun_name)
+    client = queryUtility(IMemcachedClient)
+    return MemcacheAdapter(client, globalkey=fun_name)
 
 
 directlyProvides(choose_cache, ICacheChooser)
